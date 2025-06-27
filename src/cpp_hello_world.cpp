503ded9ae7d88114580e8cd87f553e0b75c7d7d0
sjharb <zzmharbison@gmail.com>
2025-06-27 16:41:33 -0500

    First cpp hello world program commit.

---
+++ src/cpp_hello_world.cpp
@@ -0,0 +1,12 @@
+// C++ hello world example project
+#include <iostream>
+using namespace std;
+
+// main function: execution starts here.
+int main() {
+    // print some output to the console or command line.
+    cout << "Hello World!";
+
+    // return status code 0 for success.
+    return 0;
+}
